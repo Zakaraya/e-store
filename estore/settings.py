94f46dd05f8d962350d1794cd24dbadcc7c6f848
Boris Zakaraya <zakaraya2000@mail.ru>
2021-01-25 15:58:59 +0300

    Деплой в Heroku

--- estore/settings.py
+++ estore/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,6 +90,12 @@ DATABASES = {
         'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
+
+import dj_database_url
+
+
+db_from_env = dj_database_url.config()
+DATABASE['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
