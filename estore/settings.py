d588e433b8106a0208af75d13516a70ad9e2f846
Boris <44663025+Zakaraya@users.noreply.github.com>
2021-02-25 18:17:33 +0300

    Update settings.py

--- estore/settings.py
+++ estore/settings.py
@@ -158,8 +158,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_PORT = '1025'
 EMAIL_PORT = 587
 # EMAIL_HOST_USER = ''
-EMAIL_HOST_USER = 'boriskazzz12@gmail.com'
-EMAIL_HOST_PASSWORD = 'Boris1214'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
 #
