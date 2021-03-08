9beef7ea6e56e1d5ad45ceed891f34f88224f1d2
Boris Zakaraya <zakaraya2000@mail.ru>
2021-03-08 14:03:01 +0300

    изменения в settings

--- estore/settings.py
+++ estore/settings.py
@@ -51,8 +51,7 @@ INSTALLED_APPS = [
 
     # 'django_elasticsearch_dsl',
     'django_filters'
-    
-    
+
 ]
 
 MIDDLEWARE = [
@@ -180,10 +179,9 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_LOCATION = 'static'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'       ---------- не забыть раскомментировать
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'  # ---------- не забыть раскомментировать
 # AWS_MEDIA_URL = "{}/{}/".format(AWS_URL, AWS_STORAGE_BUCKET_NAME)
 # MEDIA_URL = AWS_MEDIA_URL
-
 
 #  Bonsai Elasticsearch
 # ES_URL = urlparse(os.environ.get('BONSAI_URL') or 'http://127.0.0.1:9200/')
