c76b3f382f45bcfc4769b4cd35dbf6662bcb8855
Boris Zakaraya <zakaraya2000@mail.ru>
2021-03-08 14:01:32 +0300

    изменения в settings

--- estore/settings.py
+++ estore/settings.py
@@ -176,12 +176,11 @@ AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-# AWS_URL = 'https://izugdidi-django-static.s3.eu-north-1.amazonaws.com/'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_LOCATION = 'static'
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'       ---------- не забыть раскомментировать
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'       ---------- не забыть раскомментировать
 # AWS_MEDIA_URL = "{}/{}/".format(AWS_URL, AWS_STORAGE_BUCKET_NAME)
 # MEDIA_URL = AWS_MEDIA_URL
 
